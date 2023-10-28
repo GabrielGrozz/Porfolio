ca24ccc8706bbae44c747da8dd82736ab9559cbd
GabrielGrozz <gabrielgrozz.net@gmail.com>
2023-10-28 14:16:45 -0300

    add photo and carrousel

---
+++ Portfolio/Models/Project.cs
@@ -0,0 +1,11 @@
+﻿namespace Portfolio.Models
+{
+    public class Project
+    {
+        public int ProjectId { get; set; }
+        public string ProjectName { get; set; }
+        public string ProjectDescription { get; set; }
+
+
+    }
+}
---
+++ Portfolio/Models/Technology.cs
@@ -0,0 +1,11 @@
+﻿namespace Portfolio.Models
+{
+    public class Technology
+    {
+        public int TechnologyId {get;set;}
+        public string TechnologyName { get;set;}
+        public string TechnologyDescription { get;set;}
+        public string TechnologyDocumentation { get;set;}
+
+    }
+}
